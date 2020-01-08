95c56480da2eb7b2a0f1b95a5733e9c2609fa865
Alec Skipper <aleskipp@uat.edu>
2020-01-08 13:22:13 -0700

    CharacterController and TankData

---
+++ Assets/Scripts/TankData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankData : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotateSpeed;
+}
---
+++ Assets/Scripts/TankMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TankMove : MonoBehaviour
+{
+    //Creating CharacterController and Transform components,
+    //The Transform is a small optimization of code for the functions.
+    private CharacterController characterController;
+    private Transform tf;
+
+    private void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Move(float speed)
+    {
+        Vector3 speedVector = tf.forward * speed;
+
+        characterController.SimpleMove(speedVector);
+    }
+
+    public void Rotate(float speed)
+    {
+        Vector3 rotateVector = Vector3.up * speed * Time.deltaTime;
+
+        tf.Rotate(rotateVector, relativeTo: Space.Self);
+    }
+}
